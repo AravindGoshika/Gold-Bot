de29a238ecd41eafe24d73786b79f841754636d6
AravindGoshika <92818622+AravindGoshika@users.noreply.github.com>
2026-09-25 10:28:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Gold Bot Live!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+Thread(target=run).start()
 import telebot, random, time, threading, os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
